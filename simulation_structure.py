1a8fc30174293396bb06e843430c995400c07693
julialruiter <julialruiter@gmail.com>
2022-05-21 23:18:59 +0200

    node edge refernce mappoing

--- simulation_structure.py
+++ simulation_structure.py
@@ -4,23 +4,40 @@ class TrafficManager():
     def __init__(self, network_manager):
         self.nm = network_manager
 
+    def tick():
+        pass
+
 
 class NetworkManager():
     def __init__(self, network_config, car_config):
-        self.node_list = []
-        self.edge_list = []
+        self.node_id_to_node_mapping = {}
+        self.edge_id_to_edge_mapping = {}
 
         for node_entry in network_config["node_list"]:
-            self.node_list.append(Node(node_entry))
-        print(self.node_list)
+            new_node = Node(node_entry)
+            self.node_id_to_node_mapping[new_node.id] = new_node
+        print(self.node_id_to_node_mapping)
 
         for edge_entry in network_config["edge_list"]:
-            self.edge_list.append(Edge(edge_entry))
-        print(self.edge_list)
+            new_edge = Edge(edge_entry)
+
+            self.edge_id_to_edge_mapping[new_edge.id] = new_edge
+
+            from_node = self.node_id_to_node_mapping[new_edge.start_node]
+            from_node.outbound_edge_id_to_edge_mapping[new_edge.id] = new_edge
+
+            to_node = self.node_id_to_node_mapping[new_edge.end_node]
+            to_node.inbound_edge_id_to_edge_mapping[new_edge.id] = new_edge
+        print(self.edge_id_to_edge_mapping)
+
+
+
 
 class Node():
     def __init__(self, config) -> None:
         self.id = config["node_ID"]
+        self.inbound_edge_id_to_edge_mapping = {}
+        self.outbound_edge_id_to_edge_mapping = {}
 
 class Edge():
     def __init__(self, config) -> None:
